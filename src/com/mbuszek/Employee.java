2e3f6963b1fb87a1d319253f9d52f462a1ccdfe5
MiroslawBuszek <zoowett@gmail.com>
2019-01-28 12:14:06 +0100

    Deleted unuseful code

--- src/com/mbuszek/Employee.java
+++ src/com/mbuszek/Employee.java
@@ -2,27 +2,20 @@ package com.mbuszek;
 
 public class Employee {
 
-
     private String firstName;
     private String secondName;
-    private String emailAdress;
+    private String emailAddress;
     private String phoneNumber;
     private int salary;
     private int age;
 
-
     public Employee(String firstName, String secondName, String emailAdress, String phoneNumber, int salary, int age) {
         this.firstName = firstName;
         this.secondName = secondName;
-        this.emailAdress = emailAdress;
+        this.emailAddress = emailAdress;
         this.phoneNumber = phoneNumber;
         this.salary = salary;
         this.age = age;
-    }
-
-    public void setAge(int age) {
-        this.firstName = firstName;
-
     }
 
     public int getAge() {
@@ -30,12 +23,11 @@ public class Employee {
     }
 
     public void generalInfo() {
-        System.out.println("Full name: " + firstName + " " + secondName + "\n" +
-                           "Age:" + age + "\n" +
-                           "Email: " + emailAdress + "\n" +
-                           "Phone: " + phoneNumber + "\n" +
-                           "Salary: " + salary);
-
+        System.out.println(
+                        "Full name: " + firstName + " " + secondName + "\n" +
+                        "Age: " + age + "\n" +
+                        "Email: " + emailAddress + "\n" +
+                        "Phone: " + phoneNumber + "\n" +
+                        "Salary: " + salary);
     }
-
 }
